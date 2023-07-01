f3b239275ccf55f552829de9cd1b13e4c86f103b
CMoralesP <cristobal.morales@miningtag.cl>
2023-07-01 16:14:20 -0400

    Add table to web

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import streamlit as st
+import pandas as pd
 
 st.title("Resumen diario de correos priorizados")
 
 st.markdown("Esta app mostrará el resumen diario")
+
+df_priority = pd.read_csv("data.csv")
+
+st.table(data=df_priority)
